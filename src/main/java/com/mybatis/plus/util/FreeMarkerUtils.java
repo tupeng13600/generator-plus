6b284be9eff906a4ac62530943d0bb3276b42444
tutu <txx170816@sina.com>
2018-11-04 14:12:03 +0800

    init project

--- src/main/java/com/mybatis/plus/util/FreeMarkerUtils.java
+++ src/main/java/com/mybatis/plus/util/FreeMarkerUtils.java
@@ -1,14 +1,16 @@
 package com.mybatis.plus.util;
 
+import com.mybatis.plus.Generator;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 
 import java.io.*;
+import java.net.URL;
 import java.util.Map;
 
 public abstract class FreeMarkerUtils {
 
-    private static final String TEMPLATE_PATH = "src/main/resources/template";
+    private static final String TEMPLATE_PATH = "/template";
 
     public static void writeFiles(String filePath, String templateName, Map<String, Object> dataMap, Boolean override) {
 
@@ -22,7 +24,8 @@ public abstract class FreeMarkerUtils {
         Configuration configuration = new Configuration(Configuration.VERSION_2_3_0);
         Writer out = null;
         try {
-            configuration.setDirectoryForTemplateLoading(new File(TEMPLATE_PATH));
+            URL url = FreeMarkerUtils.class.getResource(TEMPLATE_PATH);
+            configuration.setDirectoryForTemplateLoading(new File(url.getFile()));
             Template template = configuration.getTemplate(templateName);
             File desFile = FileUtil.createIfNotExist(desPath);
             out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(desFile)));
@@ -39,5 +42,15 @@ public abstract class FreeMarkerUtils {
             }
         }
     }
+
+//    public static void main(String[] args) {
+//        Generator.build().ip("127.0.0.1")
+//                .port(3306)
+//                .database("sjs")
+//                .user("root")
+//                .password("Txx@13600")
+//                .repositoryPackage("com.demo.repository")
+//                .gernerate();
+//    }
 
 }
